1f9fc0cad7404fee0e4e9a461729b6a2a0658fb7
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-12 20:09:46 -0300

    auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -110,7 +110,7 @@ class UserController extends Controller
                 'email' => $user->email,
                 'name' => $user->name,
                 'logo' => $user->logo,
-                /* 'token' => $token, */
+                'token' => $token,
 
             ]
         
