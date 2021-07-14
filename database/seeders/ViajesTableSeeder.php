51ed79ecb768a1a5b992550aad7a22a26ae3ac8d
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-13 22:27:29 -0300

    auth

--- database/seeders/ViajesTableSeeder.php
+++ database/seeders/ViajesTableSeeder.php
@@ -46,7 +46,7 @@ class ViajesTableSeeder extends Seeder
             'fecha_salida' => "2021-10-22 23:18:18",
             'fecha_llegada' => "2021-11-22 10:20:20",
             'cantidad_asientos' =>'40',
-            'precio' => 4000
+            'precio' => 5000
 
         ]);
 
@@ -58,7 +58,7 @@ class ViajesTableSeeder extends Seeder
             'fecha_salida' => "2021-10-22 17:30:18",
             'fecha_llegada' => "2021-11-22 03:20:20",
             'cantidad_asientos' =>'40',
-            'precio' => 4000
+            'precio' => 3500
 
         ]);
 
@@ -70,7 +70,7 @@ class ViajesTableSeeder extends Seeder
             'fecha_salida' => "2021-10-22 20:30:18",
             'fecha_llegada' => "2021-11-22 05:20:20",
             'cantidad_asientos' =>'40',
-            'precio' => 4000
+            'precio' => 5000
 
         ]);
 
@@ -82,7 +82,7 @@ class ViajesTableSeeder extends Seeder
             'fecha_salida' => "2021-10-22 19:30:18",
             'fecha_llegada' => "2021-11-22 05:30:20",
             'cantidad_asientos' =>'40',
-            'precio' => 4000
+            'precio' => 3500
 
         ]);
     }
