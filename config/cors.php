fe1b5c3504f67bae8820e507c1fa8fbd31dba348
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-18 18:37:04 -0300

    auth

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 
-    'allowed_origins' => ['https://andiamo-viajes.netlify.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
