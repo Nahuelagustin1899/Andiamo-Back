41df5d5dcd68311c1ef3d3c76cf5b99ebdae0f77
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-08-06 14:58:48 -0300

    reservas

--- app/Http/Controllers/ReservaController.php
+++ app/Http/Controllers/ReservaController.php
@@ -26,14 +26,14 @@ class ReservaController extends Controller
     public function indexEmpresa()
     {   
         
-       /*  $reservas = Empresa::with(['viajes', 'reservas'])->where('id', 5)->orderBy('reservas.asiento_reservado')->get(); */
+        $reservas = Empresa::with(['viajes', 'reservas'])->where('id', 5)->orderBy('reservas.asiento_reservado')->get();
 
-       $reservas = Empresa::with( [ 'viajes', 'reservas' ] )
+       /* $reservas = Empresa::with( [ 'viajes', 'reservas' ] )
             ->select( 'empresas.*', 'r.asiento_reservado' )
             ->join( 'viajes as v', 'v.empresa_id', '=', 'empresas.id' )
             ->join( 'reservas as r', 'v.id', '=', 'r.viaje_id' )
             ->where( 'empresas.id', 5 )
-            ->orderBy( 'r.asiento_reservado' )->get();
+            ->orderBy( 'r.asiento_reservado' )->get(); */
 
         return response()->json(['data' => $reservas]);
     }
